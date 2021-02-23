aa52582b57d950b5f1e58f256fbdf7c33ff813b4
Toni10 <toni.djordjevic91@gmail.com>
2021-02-23 20:27:02 +0100

    Add project files.

---
+++ Cas27/APITests.cs
@@ -0,0 +1,186 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.Http;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Cas27
+{
+    class APITests
+    {
+
+        private ContactsAPI API;
+
+        [SetUp]
+        public void Boot()
+        {
+            API = new ContactsAPI(
+                "https://api.qa.rs/api/",
+                TimeSpan.FromSeconds(10)
+            );
+        }
+
+
+        [Test]
+        public void TestGetContacts()
+        {
+            ContactsObject Contacts;
+            HttpResponseMessage Response;
+
+            API.GetContacts(
+                1,
+                out Contacts,
+                out Response
+            );
+
+            Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+            Assert.AreEqual(25, Contacts.results_per_page);
+        }
+        
+        [Test]
+        public void TestGetContact()
+        {
+            ContactObject Contact;
+            HttpResponseMessage Response;
+
+            API.GetContact(1, out Contact, out Response);
+
+            Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+            Assert.AreEqual(1, Contact.id);
+        }
+
+        [Test]
+        public void TestPostContact()
+        {
+            ContactObject Contact;
+            HttpResponseMessage Response;
+
+            ContactObject NewContact = new ContactObject();
+            NewContact.first_name = "Dejana";
+            NewContact.last_name = "Dejanovic";
+            NewContact.email = "ddejana@example.org";
+            NewContact.phone_number = "012/345-678";
+
+            API.PostContact(NewContact, out Contact, out Response);
+
+            Assert.AreEqual(HttpStatusCode.Created, Response.StatusCode);
+
+            ContactObject VerifyContact;
+
+            API.GetContact(Contact.id, out VerifyContact, out Response);
+
+            Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+            Assert.AreEqual(VerifyContact.first_name, NewContact.first_name);
+            Assert.AreEqual(VerifyContact.last_name, NewContact.last_name);
+            Assert.AreEqual(VerifyContact.email, NewContact.email);
+            Assert.AreEqual(VerifyContact.phone_number, NewContact.phone_number);
+        }
+
+        [Test]
+        [TestCase(null)]
+        public void TestUpdateContact(int? ContactId = null)
+        {
+            ContactObject Contact;
+            ContactsObject Contacts;
+            HttpResponseMessage Response;
+
+            if (!ContactId.HasValue) {
+                API.GetContacts(1, out Contacts, out Response);
+                Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+                ContactId = Contacts.results.First().id;
+            }
+
+            API.GetContact(ContactId.Value, out Contact, out Response);
+            Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+
+            Contact.phone_number = "000-0000-000";
+
+            API.PutContact(ContactId.Value, Contact, out Response);
+            Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+        }
+
+        [Test]
+        [TestCase(null)]
+        public void TestDeleteContact(int? ContactId = null)
+        {
+            ContactObject Contact;
+            ContactsObject Contacts;
+            HttpResponseMessage Response;
+
+            if (!ContactId.HasValue)
+            {
+                API.GetContacts(1, out Contacts, out Response);
+                Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+                ContactId = Contacts.results.First().id;
+            }
+
+            TestContext.WriteLine("Trying to delete user with id: {0}", ContactId);
+            API.DeleteContact(ContactId.Value, out Response);
+            Assert.AreEqual(HttpStatusCode.NoContent, Response.StatusCode);
+            
+            API.GetContact(ContactId.Value, out Contact, out Response);
+            Assert.AreEqual(HttpStatusCode.NotFound, Response.StatusCode);
+            TestContext.WriteLine("Successfully deleted user with id: {0}", ContactId);
+        }
+
+        [Test]
+        public void TestCreateUpdateDeleteUser()
+        {
+            ContactObject Contact;
+            ContactObject VerifyContact;
+            ContactObject UpdateContact = new ContactObject();
+            ContactObject NewContact = new ContactObject();
+            HttpResponseMessage Response;
+
+            NewContact.first_name = "Bob";
+            NewContact.last_name = "Buttons";
+            NewContact.email = "bob.buttons@example.net";
+            NewContact.phone_number = "1-800-555-12345";
+
+            API.PostContact(NewContact, out Contact, out Response);
+            Assert.AreEqual(HttpStatusCode.Created, Response.StatusCode);
+            TestContext.WriteLine("Created user with id: {0}", Contact.id);
+
+            API.GetContact(Contact.id, out VerifyContact, out Response);
+            Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+            Assert.AreEqual(VerifyContact.first_name, NewContact.first_name);
+            Assert.AreEqual(VerifyContact.last_name, NewContact.last_name);
+            Assert.AreEqual(VerifyContact.email, NewContact.email);
+            Assert.AreEqual(VerifyContact.phone_number, NewContact.phone_number);
+
+            UpdateContact = VerifyContact;
+            UpdateContact.phone_number = "1-800-555-6666";
+
+            API.PutContact(UpdateContact.id, UpdateContact, out Response);
+            Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+            TestContext.WriteLine("Updated user {0} information", UpdateContact.id);
+
+            API.GetContact(UpdateContact.id, out VerifyContact, out Response);
+            Assert.AreEqual(HttpStatusCode.OK, Response.StatusCode);
+            Assert.AreEqual(VerifyContact.first_name, UpdateContact.first_name);
+            Assert.AreEqual(VerifyContact.last_name, UpdateContact.last_name);
+            Assert.AreEqual(VerifyContact.email, UpdateContact.email);
+            Assert.AreEqual(VerifyContact.phone_number, UpdateContact.phone_number);
+            TestContext.WriteLine("Verified user {0} information update", UpdateContact.id);
+
+            Thread.Sleep(1000);
+
+            API.DeleteContact(UpdateContact.id, out Response);
+            Assert.AreEqual(HttpStatusCode.NoContent, Response.StatusCode);
+
+            API.GetContact(UpdateContact.id, out Contact, out Response);
+            TestContext.Write("Deleting user with id: {0} ... ", UpdateContact.id);
+            Assert.AreEqual(HttpStatusCode.NotFound, Response.StatusCode);
+            TestContext.Write("Deleted");
+        }
+
+
+        [TearDown]
+        public void Destroy()
+        {
+            API.Cleanup();
+        }
+    }
+}
---
+++ Cas27/ContactsAPI.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Cas27
+{
+    class ContactsAPI
+    {
+        private HttpClient Client;
+
+        public ContactsAPI(String BaseURL, TimeSpan Timeout)
+        {
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri(BaseURL);
+            Client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+            );
+            Client.Timeout = Timeout;
+        }
+
+        public void GetContacts(int Page, out ContactsObject Contacts, out HttpResponseMessage Response)
+        {
+            Response = Client.GetAsync("contacts/" + Page.ToString()).Result;
+            if (Response.IsSuccessStatusCode)
+            {
+                Contacts = Response.Content.ReadAsAsync<ContactsObject>().Result;
+            } else
+            {
+                Contacts = new ContactsObject();
+            }
+        }
+
+        public void GetContact(int Id, out ContactObject Contact, out HttpResponseMessage Response)
+        {
+            Response = Client.GetAsync("contact/" + Id.ToString()).Result;
+            if (Response.IsSuccessStatusCode)
+            {
+                Contact = Response.Content.ReadAsAsync<ContactObject>().Result;
+            } else
+            {
+                Contact = new ContactObject();
+            }
+        }
+
+        public void PostContact(
+            ContactObject NewContact,
+            out ContactObject InsertedContact,
+            out HttpResponseMessage Response
+        ) {
+            Response = Client.PostAsJsonAsync<ContactObject>("contact", NewContact).Result;
+            if (Response.IsSuccessStatusCode)
+            {
+                InsertedContact = Response.Content.ReadAsAsync<ContactObject>().Result;
+            }
+            else
+            {
+                InsertedContact = new ContactObject();
+            }
+        }
+
+        public void PutContact(int Id, ContactObject UpdatedContact, out HttpResponseMessage Response)
+        {
+            Response = Client.PutAsJsonAsync<ContactObject>("contact/" + Id.ToString(), UpdatedContact).Result;
+        }
+
+        public void DeleteContact(int Id, out HttpResponseMessage Response)
+        {
+            Response = Client.DeleteAsync("contact/" + Id.ToString()).Result;
+        }
+
+        public void Cleanup()
+        {
+            Client.Dispose();
+        }
+    }
+}
